68f116e1b9302aea21c004f4eb2804ec12cd011a
Adam Culp <adamculp@uws.net>
2013-11-14 20:13:43 -0500

    Added docblocks to Application module functions

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -14,11 +14,17 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+    /**
+     * @return array|ViewModel
+     */
     public function indexAction()
     {
         return new ViewModel();
     }
-    
+
+    /**
+     * @return ViewModel
+     */
     public function fooAction()
     {
         return new ViewModel();
